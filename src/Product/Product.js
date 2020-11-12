f7b52ffc6d1852fb703d30eca450969146fde841
dstrivedi <drashti1118@gmail.com>
2020-11-12 16:33:08 +0530

    1 file modified, 1 added, 1 deleted

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
+import productStyles from "./product.module.css";
 
 class Product extends Component {
-  productData = [];
+  state = {
+    productData: [],
+    isError: false
+  };
 
   componentDidMount() {
     const url =
       "https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json";
     fetch(url)
+      // We get the API response and receive data in JSON format...
       .then(res => res.json())
+      // ...then we update the users state
       .then(data => {
-        this.productData = data;
-        console.log("Data: ", this.productData);
-      });
+        this.setState({ productData: data.groups });
+        console.log("Data: ", this.state.productData);
+      })
+      .catch(error => this.setState({ isError: error }));
   }
 
   render() {
     return (
       <div>
         <p>
-          {this.productData.map(p => {
-            <li>{p.id}</li>;
+          {this.state.productData.map(p => {
+            return (
+              <div className={productStyles.details}>
+                <a
+                  // style={{ color: "black", textDecoration: "none" }}
+                  href={p.links.www}
+                  target="_blank"
+                >
+                  <p>{p.name}</p>
+                </a>
+                <img src={p.hero.href} />
+                <p>$</p>
+              </div>
+            );
           })}
         </p>
       </div>
