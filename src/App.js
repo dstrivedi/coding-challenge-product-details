462810d740292735bbd43ac6995a32b6cc66f695
dstrivedi <drashti1118@gmail.com>
2020-11-12 18:35:22 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-import Products from "./Products/Products.js";
+import Products from "./Product-Details/Products/Products.js";
 
 export default function App() {
   return (
--- src/Product-Details/Product/Product.js
+++ src/Product-Details/Product/Product.js
@@ -4,23 +4,21 @@ import productStyles from "./product.module.css";
 const Product = props => {
   return (
     <div>
-      <p>
-        {props.product.map(p => {
-          return (
-            <div className={productStyles.details}>
-              <a
-                // style={{ color: "black", textDecoration: "none" }}
-                href={p.links.www}
-                target="_blank"
-              >
-                <p>{p.name}</p>
-              </a>
-              <img src={p.hero.href} />
-              <p>$</p>
-            </div>
-          );
-        })}
-      </p>
+      {props.product.map(p => {
+        return (
+          <div className={productStyles.details}>
+            <a
+              // style={{ color: "black", textDecoration: "none" }}
+              href={p.links.www}
+              target="_blank"
+            >
+              <p>{p.name}</p>
+            </a>
+            <img src={p.hero.href} />
+            <p>$</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
