27d2e7a76658ba20ff00305f84be10c26615d338
dstrivedi <drashti1118@gmail.com>
2020-11-14 13:24:29 +0530

    2 files modified

--- src/Product-Details/Product/Product.js
+++ src/Product-Details/Product/Product.js
@@ -8,17 +8,35 @@ const Product = props => {
         return (
           <div className={productStyles.product}>
             <a
-              style={{ color: "black", textDecoration: "none" }}
+              style={{
+                color: "black",
+                textDecoration: "none",
+                position: "absolute",
+                fontWeight: "bold",
+                maxWidth: "100%",
+                width: "130vh",
+                backgroundColor: "white",
+                opacity: "0.5"
+              }}
               href={p.links.www}
               target="_blank"
             >
               <p>{p.name}</p>
             </a>
-            <img
-              style={{ maxWidth: "100%", height: "100%" }}
-              src={p.hero.href}
-            />
-            <p>$799</p>
+            <img style={{ width: "100%", height: "100%" }} src={p.hero.href} />
+            <p
+              style={{
+                position: "absolute",
+                marginTop: "-50px",
+                marginLeft: "20px",
+                padding: "5px",
+                backgroundColor: "black",
+                color: "white",
+                opacity: "0.6"
+              }}
+            >
+              $799
+            </p>
           </div>
         );
       })}
